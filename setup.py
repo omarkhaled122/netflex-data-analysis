f9a1dcebc3160f1b5a5ff6b6fbdf5939dc9875dc
omarkhaled122 <khaledo32.ok@gmail.com>
2024-12-26 17:07:34 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing movies and TV-shows from netflix dataset',
+    author='EpsilonAI',
+    license='',
+)
